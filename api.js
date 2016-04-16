d0b9a0baa48c75a6428203f8df4bff46e7942007
juanvallejo <juan.vallejo.12@cnu.edu>
2016-04-16 16:25:36 -0400

    final commit

--- api.js
+++ api.js
@@ -2,6 +2,21 @@ var fs         = require('fs');
 var spawn      = require('child_process').spawn;
 var Multiparty = require('multiparty');
 var db         = require('./db.js');
+var atob = require('atob');
+
+function decodeBase64Image(dataString) {
+  var matches = dataString.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/),
+    response = {};
+
+  if (matches && matches.length !== 3) {
+    return new Error('Invalid input string');
+  }
+
+  response.type = matches[1];
+  response.data = new Buffer(matches[2], 'base64');
+
+  return response;
+}
 
 var api = {
 
@@ -30,8 +45,29 @@ var api = {
         return form.parse(req, function(err, fields, files) {
 
             if(err) {
+            	console.log(req.headers);
                 console.log('ERR MULTIPARTY', err);
                 return callback.call(this, 'err_upload');
+            }
+
+            var length = 0;
+            for(var i in files) {
+            	length++;
+            }
+
+            if(!length) {
+            	files = fields;
+				// var base64Data = files.image[0].replace(/^data:image\/png;base64,/, "");
+				console.log(files.image);
+				decodeBase64Image(files.image[0]);
+				// save file
+                // fs.writeFile(__dirname + '/images/image.png', base64Data, 'base64', function(err) {
+                // 	callback.call(this, null, {
+	               //  	img_path: '/images/' + files.image[0].originalFilename,
+	               //  	username: fields.username
+	               //  });
+                // });
+            	return;
             }
 
             if(!files.image) {
@@ -84,6 +120,27 @@ var api = {
 			return api.respond_err(res, 'Invalid ');
 		}
 
+		var textpath = 'sample1.txt';
+		api.extract_img_text(__dirname + '/doc.jpg', function(err, ocrData) {
+			// save extracted text
+			fs.writeFile(__dirname + textpath, ocrData, function(err) {
+
+				api.respond_ok(res, null,
+					{
+						error: false,
+						message: 'success',
+						img_path: '/images/path',
+						ocr_path: textpath,
+						has_ocr: true,
+						ocr: ocrData,
+						published_at: Date.now()
+					}
+				);
+
+			});
+		});
+		return;
+
 		api.receive_res_multipart(req, function(err, data) {
 
 			if(err) {
@@ -105,15 +162,17 @@ var api = {
 				// save extracted text
 				fs.writeFile(__dirname + textpath, ocrData, function(err) {
 
-					api.respond_ok(res, null, [
+					api.respond_ok(res, null,
 						{
+							error: false,
+							message: 'success',
 							img_path: data.img_path,
 							ocr_path: textpath,
 							has_ocr: true,
 							ocr: ocrData,
 							published_at: Date.now()
 						}
-					]);
+					);
 
 				});
 			});
@@ -136,7 +195,20 @@ var api = {
 	},
 
 	get_user_img: function(req, res) {
-		res.end('test');
+		var user = req.url.split('/');
+		user = user[user.length - 1];
+
+		db.selectFrom('files', ['*'], 'username="' + user + '"', function(err, rows) {
+			api.respond_ok(res, null,
+				{
+					error: false,
+					message: 'success',
+					user: user,
+					uploads: rows
+				}
+			);
+		});
+
 	}
 
 };
--- server.js
+++ server.js
@@ -4,8 +4,26 @@
 var PORT = 8000;
 var HOST = '0.0.0.0';
 
+var fs = require('fs');
 var http = require('http');
 var api = require('./api.js');
+
+var dictionaryOfMimeTypes = {
+
+    'css'   : 'text/css'                ,
+    'html'  : 'text/html'               ,
+    'ico'   : 'image/x-icon'            ,
+    'jpg'   : 'image/jpeg'              ,
+    'jpeg'  : 'image/jpeg'              ,
+    'js'    : 'application/javascript'  ,
+    'map'   : 'application/x-navimap'   ,
+    'pdf'   : 'application/pdf'         ,
+    'png'   : 'image/png'               ,
+    'ttf'   : 'application/octet-stream',
+    'txt'   : 'text/plain'              ,
+    'woff'  : 'application/x-font-woff'
+
+};
 
 var api_routes = {
 	'/api/post': {
@@ -30,18 +48,29 @@ var app = http.createServer(function(req, res) {
 
 	if(req.url == '/') {
 		res.writeHead(200, { 'Content-Type': 'text/html' });
-		res.end('<form method="POST" action="/api/post" enctype="multipart/form-data"> \
+		return res.end('<form method="POST" action="/api/post" enctype="multipart/form-data"> \
 			<input type="file" name="image" /><br /> \
 			<input type="text" name="username" placeholder="Enter your username" /> \
 			<input type="submit" value="submit" /> \
 		</form>');
 	}
  
-	res.writeHead(500);
-	res.end(JSON.stringify({
-	    "error": "true",
-	    "message": "Invalid endpoint"
-	}));
+	fs.readFile(__dirname + req.url, function(err, data) {
+		
+		if(err) {
+			res.writeHead(500);
+			return res.end(JSON.stringify({
+			    "error": "true",
+			    "message": "Invalid endpoint"
+			}));
+		}
+
+		var ext = req.url.split('.');
+		ext = ext[ext.length - 1];
+		res.writeHead(200, {'Content-Type': dictionaryOfMimeTypes[ext]});
+		res.end(data);
+
+	});
 
 }); 
 
